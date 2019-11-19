05c4772839e5f3381841f866592ca8cb9e484a40
Alfie Gibbs <alfie.gibbs@outlook.com>
2019-11-19 11:53:03 +0000

    added create trip method

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,11 +5,17 @@ class TripsController < ApplicationController
 
   def create
     @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    if @trip.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
   def trip_params
-    params.require(:trip).permit(:name, :permit)
+    params.require(:trip).permit(:destination, :origin, :departure_time, :seats)
   end
 end
