0e95bd642340ee81813b618b6a388995fccdc928
Julian Steinmann <steinmann.julian@gmail.com>
2019-11-21 14:07:26 +0000

    Fix redirect of profile edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
   root to: 'pages#home'
   get '/dashboard', to: 'pages#dashboard'
   resources :trips, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create]
   end
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
