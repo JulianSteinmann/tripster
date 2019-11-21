5c916c39d92f5a86314367755e42643abe0e9754
Julian Steinmann <steinmann.julian@gmail.com>
2019-11-21 14:41:38 +0000

    unfinished edits to the dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,8 @@ class PagesController < ApplicationController
   def dashboard
     @trips = Trip.all
     @my_trips = Trip.where(user_id: current_user.id)
-    bookings = Booking.joins(:trip).where(user_id: current_user.id)
-    @booked_trips = bookings.map(&:trip)
+    @bookings = Booking.joins(:trip).where(user_id: current_user.id)
+    # @booked_trips = bookings.map(&:trip)
     @user = current_user
   end
 end
